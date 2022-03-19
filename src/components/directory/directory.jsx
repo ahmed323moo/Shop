8a3f2a5fdbd35406a46ddc1c47627c819bdd7383
ahmed323moo <79893164+ahmed323moo@users.noreply.github.com>
2022-03-19 17:56:48 +0300

    -v

---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from "../category-item/category-item";
+import "./directory.scss";
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => {
+        return (
+          <CategoryItem category={category} key={category.id}></CategoryItem>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Directory;
